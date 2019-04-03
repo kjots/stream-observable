a796812136a0ef93158efa3ff25bf2950d6e4786
Karl Juri Ots <kjots@kjots.org>
2019-04-03 16:30:11 +1100

    Updated to use custom stream to observable logic instead of stream-to-observable package.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
-import streamToObservable from 'stream-to-observable';
+import { Writable } from 'stream';
 
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
 
 import ReadableStream = NodeJS.ReadableStream;
 
 export function streamObservable<T>(stream: ReadableStream): Observable<T> {
-  return streamToObservable(stream);
+  return new Observable((observer: Observer<T>) => {
+    const observerWritable = new Writable({
+      objectMode: true,
+
+      write(chunk: any, encoding: string, callback: () => void) {
+        observer.next(chunk);
+
+        callback();
+      },
+
+      final(callback: () => void) {
+        observer.complete();
+
+        callback();
+      }
+    });
+
+    stream
+      .on('error', error => observer.error(error))
+      .pipe(observerWritable);
+
+    return () => stream.unpipe(observerWritable);
+  });
 }
--- types/stream-to-observable/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'stream-to-observable' {
-  import { Observable } from 'rxjs';
-
-  import ReadableStream = NodeJS.ReadableStream;
-
-  function streamToObservable<T>(stream: ReadableStream): Observable<T>;
-
-  export = streamToObservable;
-}
