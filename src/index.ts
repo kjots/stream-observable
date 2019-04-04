54da27d8ede28fcc489ddd73abd57d85d70a38e1
Karl Juri Ots <kjots@kjots.org>
2019-04-04 15:42:24 +1100

    Added StreamObservable and ObserverWritable classes.

--- src/index.ts
+++ src/index.ts
@@ -4,28 +4,50 @@ import { Observable, Observer } from 'rxjs';
 
 import ReadableStream = NodeJS.ReadableStream;
 
-export function streamObservable<T>(stream: ReadableStream): Observable<T> {
-  return new Observable((observer: Observer<T>) => {
-    const observerWritable = new Writable({
-      objectMode: true,
+export class ObserverWritable<T> extends Writable {
+  private readonly observer: Observer<T>;
 
-      write(chunk: any, encoding: string, callback: () => void) {
-        observer.next(chunk);
+  constructor(observer: Observer<T>) {
+    super({ objectMode: true });
 
-        callback();
-      },
+    this.observer = observer;
+  }
 
-      final(callback: () => void) {
-        observer.complete();
+  public onError = (error: any) => this.observer.error(error);
 
-        callback();
-      }
+  public _write(chunk: any, encoding: string, callback: () => void) {
+    this.observer.next(chunk);
+
+    callback();
+  }
+
+  public _final(callback: () => void) {
+    this.observer.complete();
+
+    callback();
+  }
+}
+
+export class StreamObservable<T> extends Observable<T> {
+  private observerWritable!: ObserverWritable<T>;
+
+  constructor(stream: ReadableStream) {
+    super((observer: Observer<T>) => {
+      this.observerWritable = new ObserverWritable<T>(observer);
+
+      stream.on('error', this.observerWritable.onError);
+      stream.pipe(this.observerWritable);
+
+      return () => {
+        stream.unpipe(this.observerWritable);
+        stream.off('error', this.observerWritable.onError);
+
+        delete this.observerWritable;
+      };
     });
+  }
+}
 
-    stream
-      .on('error', error => observer.error(error))
-      .pipe(observerWritable);
-
-    return () => stream.unpipe(observerWritable);
-  });
+export function streamObservable<T>(stream: ReadableStream): Observable<T> {
+  return new StreamObservable<T>(stream);
 }
